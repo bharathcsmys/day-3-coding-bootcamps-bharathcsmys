6ee8d416958bfcbeb457e1a1c1ac01709c7dc43d
bharathcsmys <20cse0135@vvce.ac.in>
2021-02-17 12:24:21 +0000

    program is done

--- Problem_3.c
+++ Problem_3.c
@@ -5,9 +5,11 @@ int main() {
     int number;
     printf("Enter an integer: ");
     scanf("%d", &number);
-
-    // Write your code here
-    
+ // Write your code here
+   if (number%2==0)
+   printf("the given number is even");
+   else
+   printf("the given number is odd"); 
     
    
     return 0;
